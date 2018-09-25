53b32042578bbe7fc333889f9ac4173be9ac0019
JaimeGuti <jaime.m.guti@gmail.com>
2018-09-25 11:51:06 +0200

    Se ha realizado la importación de calcoohija y se han definido los operandos y el operador

--- calcplus.py
+++ calcplus.py
@@ -2,51 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import calcoohija
 
 fichero = open(sys.argv[1],"r")
 lineas = fichero.readlines()
 
-class Calculadora(object):
-    def plus(self, op1, op2):
-        """ Function to sum the operands. Ops have to be ints """
-        return op1 + op2
+for valor in lineas:
 
-    def minus(self, op1, op2):
-        """ Function to substract the operands """
-        return op1 - op2
+    operador = valor.rsplit(",")[0]
+    operandos = valor.rsplit(",")[1:]
 
-class CalculadoraHija(Calculadora):
+print("ok")
 
-    def multiplicar(self, op1, op2):
-        """ Function to sum the operands. Ops have to be ints """
-        return op1 * op2
-
-    def dividir(self, op1, op2):
-        """ Function to substract the operands """
-        if op2 == 0:
-            sys.exit("Error: Division by zero is not allowed")
-        return op1 / op2
-
-
-micalc = CalculadoraHija()
-
-
-if __name__ == "__main__":
-    try:
-        operando1 = int(sys.argv[1])
-        operando2 = int(sys.argv[3])
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
-
-    if sys.argv[2] == "suma":
-        result = micalc.plus(operando1, operando2)
-    elif sys.argv[2] == "resta":
-        result = micalc.minus(operando1, operando2)
-    elif sys.argv[2] == "multiplica":
-        result = micalc.multiplicar(operando1, operando2)
-    elif sys.argv[2] == "divide":
-        result = micalc.dividir(operando1, operando2)
-    else:
-        sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir.')
-
-    print(result)
+fichero.close()
