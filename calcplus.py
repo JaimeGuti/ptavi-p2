b7cb1779fef8d4edd77ec9a9248e8fcce39ff5eb
JaimeGuti <jaime.m.guti@gmail.com>
2018-09-25 13:39:43 +0200

    Añadidas las funciones de resta, multiplicación y división de los operandos del fichero

--- calcplus.py
+++ calcplus.py
@@ -23,10 +23,23 @@ if __name__ == "__main__":
             if operador == "suma":
                 for suma in operandos:
                     result = micalc.plus((result), int(suma))
+                print(result)
+            if operador == "resta":
+                for resta in operandos:
+                    result = micalc.minus((result), int(resta))
+                print(result)
+            if operador == "multiplica":
+                for multiplicacion in operandos:
+                    result = micalc.multiplicar((result), int(multiplicacion))
+                print(result)
+            if operador == "divide":
+                for division in operandos:
+                    result = micalc.dividir((result), int(division))
+                print(result)
 
         except KeyError:
             sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir.')
 
-    print(result)
+    #print(result)
 
 fichero.close()
